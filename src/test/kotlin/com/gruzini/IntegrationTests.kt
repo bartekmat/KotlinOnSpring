94f91d09387e071bbe35575df98fa88c2659bd7f
Bartek <bart.matusz@gmail.com>
2020-11-01 00:03:49 +0100

    updated integration tests

--- src/test/kotlin/com/gruzini/IntegrationTests.kt
+++ src/test/kotlin/com/gruzini/IntegrationTests.kt
@@ -22,7 +22,19 @@ class IntegrationTests(@Autowired val restTemplate: TestRestTemplate) {
     fun `Assert blog page title, content and status code`() {
         val entity = restTemplate.getForEntity<String>("/")
         assertThat(entity.statusCode).isEqualTo(HttpStatus.OK)
-        assertThat(entity.body).contains("<h1>Blog</h1>")
+        assertThat(entity.body).contains("<h1>Kotlin Blog</h1>", "Prawda")
+    }
+    @Test
+    fun `Assert article page title, content and status code`(){
+        val title = "Prawda Futbolu"
+        val page = restTemplate.getForEntity<String>("/article/${title.toSlug()}")
+        assertThat(page.statusCode).isEqualTo(HttpStatus.OK)
+        assertThat(page.body).contains(title, "kazdy ma swoja prawde futbolu")
+    }
+    @Test
+    fun `Assert article page status code for non existing article` (){
+        val page = restTemplate.getForEntity<String>("/article/xyz")
+        assertThat(page.statusCode).isEqualTo(HttpStatus.NOT_FOUND)
     }
 
     @AfterAll
